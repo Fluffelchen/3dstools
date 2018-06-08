cef2d298200ecd22a217095f18ef01f4d25cc486
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-06-08 15:23:14 -0500

    Full list of changes below
    - Added support for converting 3DS to CIA (Makerom crashes)
    - Added support for decrypting 3DS

--- 3dstools.ps1
+++ 3dstools.ps1
@@ -40,6 +40,33 @@ function GetGM9NameForCIA {
     return $TitleID
 }
 
+function Convert3DS {
+    param([int]$To, [string]$Path)
+
+    switch ($To) {
+        0 {
+            $si = [System.Diagnostics.ProcessStartInfo]::new()
+            $si.FileName = [Environment]::CurrentDirectory + "\makerom.exe"
+            $si.Arguments = "-ccitocia `"$Path`" -ignoresign -target p"
+            $si.UseShellExecute = $false
+            $si.CreateNoWindow = $true
+            [System.Diagnostics.Process]::Start($si).WaitForExit()
+        }
+
+        1 {
+            $si = [System.Diagnostics.ProcessStartInfo]::new()
+            $si.FileName = [Environment]::CurrentDirectory + "\makerom.exe"
+            $si.Arguments = "-ccitocia `"$Path`" -ignoresign -target p"
+            $si.UseShellExecute = $false
+            $si.CreateNoWindow = $true
+            [System.Diagnostics.Process]::Start($si).WaitForExit()
+            $old = [System.IO.Path]::GetFileNameWithoutExtension($Path) + ".cci"
+            $new = [System.IO.Path]::GetFileNameWithoutExtension($Path) + ".3ds"
+            Rename-Item -Path $old -NewName $new
+        }
+    }
+}
+
 function ConvertCIA {
     param([int]$To, [string]$Path)
 
@@ -64,6 +91,49 @@ function ConvertCIA {
             $new = [System.IO.Path]::GetFileNameWithoutExtension($Path) + ".3ds"
             Rename-Item -Path $old -NewName $new
         }
+    }
+}
+
+function Decrypt3DS {
+    param([string]$Path)
+
+    $si = [System.Diagnostics.ProcessStartInfo]::new()
+    $si.FileName = [Environment]::CurrentDirectory + "\decrypt.exe"
+    $si.Arguments = "`"$Path`""
+    $si.UseShellExecute = $false
+    $si.CreateNoWindow = $true
+    $si.RedirectStandardInput = $true
+    $process = [System.Diagnostics.Process]::Start($si)
+    $process.StandardInput.WriteLine()
+    $process.WaitForExit()
+    $files = Get-ChildItem -Filter "*.ncch"
+
+    foreach ($file in $files) {
+        $info = [System.IO.FileInfo]::new($file)
+        $data = [System.IO.File]::ReadAllBytes($file)
+        $data[0x188 + 3] = 0
+        $data[0x188 + 7] = 4
+        [System.IO.File]::WriteAllBytes($file, $data)
+    }
+
+    $files_str = ""
+    $i = 0
+
+    foreach ($file in $files) {
+        $files_str += " -content `"$file`:$i`:$i`""
+        ++$i
+    }
+
+    $noext = [System.IO.Path]::GetFileNameWithoutExtension($Path)
+    $si = [System.Diagnostics.ProcessStartInfo]::new()
+    $si.FileName = [Environment]::CurrentDirectory + "\makerom.exe"
+    $si.Arguments = "-f cci -o `"$noext (Decrypted).3ds`"$files_str"
+    $si.UseShellExecute = $false
+    $si.CreateNoWindow = $true
+    [System.Diagnostics.Process]::Start($si).WaitForExit()
+
+    foreach ($file in $files) {
+        Remove-Item $file
     }
 }
 
@@ -176,14 +246,15 @@ function DownloadAndDecryptCIA {
     Remove-Item "cdn" -Recurse
     Write-Host "Decrypting..."
     DecryptCIA -Path "$name.cia"
+    return $true
 }
 
 while ($true) {
 Clear-Host
 Write-Host @"
 3dstools
-(1) Convert CIA
-(2) Decrypt CIA
+(1) Convert (Decrypted Files Only)
+(2) Decrypt
 (3) Download & Decrypt CIA
 (4) Download & Decrypt CIA list
 (5) Convert all NCCHs to CIAs
@@ -194,21 +265,43 @@ $option = [int](Read-Host "Select a option")
 if ($option -eq 1) {
     $ofd = [System.Windows.Forms.OpenFileDialog]::new()
     $ofd.InitialDirectory = Convert-Path .
-    $ofd.Filter = "CTR Importable Archive (*.cia)|*.cia"
+    $ofd.Filter = "CTR Importable Archive|*.cia|3DS File|*.3ds"
     $ofd.ShowDialog() | Out-Null
 
     if ($ofd.FileName -ne "") {
-        $format = [int](Read-Host "Format (0: CCI, 1: 3DS)")
-        Write-Host "Converting..."
-        ConvertCIA -To $format -Path $ofd.FileName
-        $noext = [System.IO.Path]::GetFileNameWithoutExtension($ofd.FileName)
+        $file = [System.IO.Path]::GetFileNameWithoutExtension($ofd.FileName)
 
-        switch ($format) {
-            0 { $noext += ".cci" }
-            1 { $noext += ".3ds" }
+        switch ([System.IO.FileInfo]::new($ofd.FileName).Extension) {
+            ".3ds" {
+                $format = [int](Read-Host "Format (0: CIA)")
+
+                if ($format -ne 0) {
+                    Write-Host "Invalid format"
+                    Pause
+                    return
+                }
+
+                Write-Host "Converting..."
+                Convert3DS -To $format -Path $ofd.FileName
+
+                switch ($format) {
+                    0 { $file += ".cia" }
+                }
+            }
+            ".cia" {
+                $format = [int](Read-Host "Format (0: CCI, 1: 3DS)")
+
+                Write-Host "Converting..."
+                ConvertCIA -To $format -Path $ofd.FileName
+
+                switch ($format) {
+                    0 { $file += ".cci" }
+                    1 { $file += ".3ds" }
+                }
+            }
         }
 
-        if (!(Test-Path $noext)) {
+        if (!(Test-Path $file)) {
             Write-Host "makerom error"
             Pause
         }
@@ -216,12 +309,18 @@ if ($option -eq 1) {
 } elseif ($option -eq 2) {
     $ofd = [System.Windows.Forms.OpenFileDialog]::new()
     $ofd.InitialDirectory = Convert-Path .
-    $ofd.Filter = "CTR Importable Archive (*.cia)|*.cia"
+    $ofd.Filter = "CTR Importable Archive|*.cia|3DS File|*.3ds"
     $ofd.ShowDialog() | Out-Null
 
     if ($ofd.FileName -ne "") {
         Write-Host "Decrypting..."
-        DecryptCIA -Path $ofd.FileName
+
+        $info = [System.IO.FileInfo]::new($ofd.FileName)
+
+        switch ($info.Extension) {
+            ".3ds" { Decrypt3DS -Path $ofd.FileName }
+            ".cia" { DecryptCIA -Path $ofd.FileName }
+        }
     }
 } elseif ($option -eq 3) {
     $tid = Read-Host "Title ID"
